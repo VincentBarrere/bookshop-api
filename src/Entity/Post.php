da4877319f67d385d959c4d084393a19ac3c0d01
VincentBarrere <vincentbarrere0@gmail.com>
2022-02-23 12:08:51 +0100

    Init and serialization

---
+++ src/Entity/Post.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+#[ApiResource(
+    itemOperations: [
+        'put',
+        'delete',
+        'get'=> [
+        'normalization_context'=>['groups'=>['read:collection','read:item','read:Post']]
+    ]],
+    denormalizationContext: ['groups'=>['write:Post']],
+    normalizationContext: ['groups'=>['read:collection']]
+)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    #[Groups(['read:collection'])]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collection', 'write:Post'])]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collection', 'write:Post'])]
+    private $slug;
+
+    #[ORM\Column(type: 'text')]
+    #[Groups(['read:item', 'write:Post'])]
+    private $content;
+
+    #[ORM\Column(type: 'datetime')]
+    #[Groups(['read:item'])]
+    private $createdAt;
+
+    #[ORM\Column(type: 'datetime')]
+    private $updatedAt;
+
+    #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'posts')]
+    #[Groups(['read:item', 'write:Post'])]
+    private $caategory;
+
+    public function __construct(){
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCaategory(): ?Category
+    {
+        return $this->caategory;
+    }
+
+    public function setCaategory(?Category $caategory): self
+    {
+        $this->caategory = $caategory;
+
+        return $this;
+    }
+}
